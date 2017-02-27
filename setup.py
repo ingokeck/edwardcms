3fa56e5b37ed5c1e2b140fb7c378b8bbf8694904
Ingo Keck <ingokeck@ingokeck.de>
2017-02-27 11:50:01 +0100

    started working on packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='edwardcms',
+      version='0.1',
+      description='A simple but extensible content management system for static wbesites. AKA static website generator.',
+      url='http://github.com/ingokeck/edwardcms',
+      author='Ingo Keck',
+      author_email='ingokeck@ingokeck.de',
+      license='AGPL',
+      packages=['src'],
+      zip_safe=False)
